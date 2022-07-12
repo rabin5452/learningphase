f416b693fc9eb063df785f99c13a7001b98515b4
rabinbhattarai <rabin5452@gmail.com>
2022-07-12 13:49:08 +0545

    updated

--- inheritance.py
+++ inheritance.py
@@ -9,9 +9,14 @@ class user:
         return "unauthorized user:"
 class teacher(user):
     def __init__(self, id, username, pwd,designation):
-        super().__init__(id, username, pwd)
+        super().__init__(id, username, pwd)## super calls parent class and initialize the parent class's data:
         self.designation=designation
 class student(user):
     def __init__(self, id, username, pwd,facultly):
         super().__init__(id, username, pwd)
         self.faculty=facultly
+
+t=teacher(1,"teacher","teacher","professor")
+uname=input("enter your usernme:")
+pwd=input("enter your password:")
+print(t.login(uname,pwd))
